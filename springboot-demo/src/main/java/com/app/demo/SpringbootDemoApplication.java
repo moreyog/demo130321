94040f59c52f1c947a7597063119e68e9a87f9a6
Yogesh More <37771780+moreyog@users.noreply.github.com>
2021-03-13 15:55:12 +0530

    Update SpringbootDemoApplication.java
    test direct commit to branch

--- springboot-demo/src/main/java/com/app/demo/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/app/demo/SpringbootDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
+	//Strating point of aaplication
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
