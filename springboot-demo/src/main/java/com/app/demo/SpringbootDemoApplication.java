65444a0fdb84e300764cdaa8c06f1b3be86f063c
Yogesh More <37771780+moreyog@users.noreply.github.com>
2021-03-13 16:10:28 +0530

    Another test of conflict2

--- springboot-demo/src/main/java/com/app/demo/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/app/demo/SpringbootDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootDemoApplication {
 
 
-	//Starting point of application
+	//Starting point of application.Another Test.
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
